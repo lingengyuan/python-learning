5718f44ff512b979c98c01a24f4f9574e2caebde
Hugh.Lin <Hugh.Lin@CNGZNITDHUGHL>
2024-07-21 12:48:18 +0800

    test loop structure

---
+++ Loop Structure.py
@@ -0,0 +1,30 @@
+import random
+total = 0
+for x in range(1,101):
+    total += x 
+print(total)
+
+total2 = 0
+for x in range(1,101,2):
+    total2 += x
+print(total2)
+
+answer = random.randint(1,100)
+counter = 0
+while True:
+    counter += 1
+    guess = int(input("please enter a number between 1 and 100: "))
+    if guess > answer:
+        print("too high")
+    elif guess < answer:
+        print("too low")
+    else:
+        print("you got it in "+str(counter)+" tries")
+        break
+print("the answer is "+str(answer) + ", you got it in "+str(counter)+" tries")
+
+
+for i in range(1,10):
+    for j in range(1,10):
+        print(str(i)+"*"+str(j)+"="+str(i*j))
+    print("\n")

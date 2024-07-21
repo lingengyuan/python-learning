470c2dbf08ab3be497c4913c11d3415b3face77b
Hugh.Lin <Hugh.Lin@CNGZNITDHUGHL>
2024-07-21 12:01:55 +0800

    this program is test some variable

---
+++ variable.py
@@ -0,0 +1,21 @@
+first_variable = 10
+second_variable = 20
+print(first_variable + second_variable)
+print(first_variable - second_variable)
+print(first_variable * second_variable)
+print(first_variable / second_variable)
+
+third_variable = 30
+fourth_variable = True
+fifth_variable = "Hello World"
+sixth_variable = 10.5
+print(type(third_variable))
+print(type(fourth_variable))
+print(type(fifth_variable))
+print(type(sixth_variable))
+print(float(first_variable))
+print(int(sixth_variable))
+print(str(sixth_variable))
+print(bool(sixth_variable))
+print(ord('A'))
+print(chr(65))

e4d5df3e7d44551eb0d26c9f328d1e00cdef77b2
Hugh.Lin <Hugh.Lin@CNGZNITDHUGHL>
2024-07-21 12:39:45 +0800

    test branch structure

---
+++ Branch structure.py
@@ -0,0 +1,13 @@
+##heron's formula
+import math
+def heron(a,b,c):
+    s = (a+b+c)/2
+    return math.sqrt(s*(s-a)*(s-b)*(s-c))
+print("please enter the sides of the triangle")
+a = int(input("a: "))
+b = int(input("b: "))
+c = int(input("c: "))
+if a+b>c and a+c>b and b+c>a:
+    print("the heron's formula return is : "+ str(heron(a,b,c)))
+else:
+    print("the sides you entered cannot form a triangle")
